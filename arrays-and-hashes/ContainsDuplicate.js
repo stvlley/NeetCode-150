a033eccec09f6fc4b687a69f14c49c296a00769b
Stephen <stvlley.amzn@gmail.com>
2022-07-19 08:49:15 -0400

    find sorted solution

---
+++ arrays-and-hashes/ContainsDuplicate.js
@@ -0,0 +1,22 @@
+// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+let nums = [1,2,3,1]
+
+var containsDuplicate = function(nums) {
+    // store values in hashtable
+    const obj = {};
+
+    for (let i = 0; i <nums.length; i++) {
+        if (obj[nums[i]]) {
+            return true
+        }
+        obj[nums[i]] = true;
+
+    }
+    return false;
+
+    
+    
+};
+
+console.log(containsDuplicate(nums))
